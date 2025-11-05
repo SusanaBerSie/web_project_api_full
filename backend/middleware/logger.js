758d7ea84d3fd5e633b88c7dc043c980a2fa7375
SusanaBS <susana.bersie33@gmail.com>
2025-11-04 23:10:21 -0500

    exportar winston en logger.js

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -11,3 +11,5 @@ const errorLogger = winston.createLogger({
   format: winston.format.json(),
   transports: [new winston.transports.File({ filename: "error.log" })],
 });
+
+module.exports = { requestLogger, errorLogger };
