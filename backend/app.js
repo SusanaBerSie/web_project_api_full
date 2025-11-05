27348b23dc4dbf116a7567b6145064b73e20b100
SusanaBS <susana.bersie33@gmail.com>
2025-11-05 16:20:03 -0500

    agregar console.log en app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,9 @@ const {
 } = require("./middleware/validation");
 const { requestLogger, errorLogger } = require("./middleware/logger");
 
-mongoose.connect("mongodb://localhost:27017/aroundb");
+mongoose.connect("mongodb://127.0.0.1:27017/aroundb").then(() => {
+  console.log("Base de datos conectada");
+});
 
 app.use(express.json());
 app.use(requestLogger);
