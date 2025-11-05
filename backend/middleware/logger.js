cca864e5720673888d3830de14751649ca1b61c1
SusanaBS <susana.bersie33@gmail.com>
2025-11-05 16:08:21 -0500

    arreglar requesLogger winston

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -1,12 +1,13 @@
 const winston = require("winston");
+const expressWinston = require("express-winston");
 
-const requestLogger = winston.createLogger({
+const requestLogger = expressWinston.logger({
   level: "info",
   format: winston.format.json(),
   transports: [new winston.transports.File({ filename: "request.log" })],
 });
 
-const errorLogger = winston.createLogger({
+const errorLogger = expressWinston.errorLogger({
   level: "error",
   format: winston.format.json(),
   transports: [new winston.transports.File({ filename: "error.log" })],
